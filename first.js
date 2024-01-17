98d766a984179fc44fab2a8d65227f22bc27a3d9
gauriwagh12 <gauriwagh2001@gmail.com>
2024-01-17 12:10:05 +0530

    javascript programs

---
+++ first.js
@@ -0,0 +1,22 @@
+
+fullname ="TONY STARK";
+console.log(fullname);
+age=24;
+price=67.9;
+x=null;
+y=undefined;
+isfollow=false;
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isfollow);
+
+x=8888
+console.log(x);
+
+let fname="Gauri";
+console.log(fname);
+
+let a;
+console.log(a);
